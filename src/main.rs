e2bc22f1bd1533a25c57199bcf4b90cc641fdcbc
alvaro@acer <alvaro@acer>
2020-02-16 18:50:08 +0100

    odio este lenguaje

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,10 @@ fn main() {
     let args : Vec<String> = env::args().collect();
     println!("Los argumentos son: {:?}", args);
 
-    use Color::*;
-    
-    let p1 = Piece{colors:[C1,C2,C3,C4,C5,C6]};
-    let p2 = p1.rotate(Direction::South);
-
-    println!("Pieza original: {:?}", p1 );
-    println!("Pieza rotada: {:?}", p2);
+   
 }
 
-#[derive(Copy,Clone,Debug)]
+#[derive(Copy,Clone,Debug,PartialEq,Eq,Hash)]
 #[repr(usize)]
 enum Color{
     C1,
@@ -37,20 +31,21 @@ enum Direction{
 }
 
 
-#[derive(Debug)]
+#[derive(Debug,Eq,PartialEq,Hash)]
 struct Piece{
     colors : [Color;6],
 }
 
 struct RotablePiece<'a>{
     pub piece: &'a Piece,
-    rotations : [&'a Piece;4]
+    rotations :  [&'a Piece;4]
 }
 
 
 
 
-impl<'a> Piece{
+
+impl Piece{
     pub fn color(&self, d: Direction) -> Color {
         self.colors[d as usize]
     }
@@ -59,7 +54,26 @@ impl<'a> Piece{
         use Direction::*;
         
         match d{
- 
+            North => Piece {
+                colors: [
+                    self.color(Up),
+                    self.color(East),
+                    self.color(Down),
+                    self.color(West),
+                    self.color(South),
+                    self.color(North),
+                ]
+            },
+            East => Piece {
+                colors: [
+                    self.color(North),
+                    self.color(Up),
+                    self.color(South),
+                    self.color(Down),
+                    self.color(West),
+                    self.color(East),
+                ]
+            },
             South => Piece {
                 colors: [
                     self.color(Down),
@@ -70,14 +84,85 @@ impl<'a> Piece{
                     self.color(South)
                 ]
             },
+            West => Piece {
+                colors: [
+                    self.color(North),
+                    self.color(Down),
+                    self.color(South),
+                    self.color(Up),
+                    self.color(East),
+                    self.color(West),
+                ]
+            },
+            
             _ => panic!("No se puede girar {:?}", d),           
         }
 
     }
 }
 
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref HASHMAP: Mutex<HashMap<Piece, &'static RotablePiece<'static>>> = {
+        let mut m = HashMap::new();
+         Mutex::new(m)
+    };    
+}
+
+
+
 impl <'a> RotablePiece<'a>{
     pub fn rotate(&self, _d: Direction ) -> &RotablePiece {
         self
     }
+
+    pub fn from(p:Piece) -> &'a RotablePiece<'a>{
+        let mut map =HASHMAP.lock().unwrap();
+        map.insert(p,RotablePiece{piece:&p});
+        panic!("Sin implementar");
+    }
+
+
+    
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use crate::Color::*;
+    use crate::Direction::*;
+    use crate::Piece;
+    use crate::RotablePiece;
+    
+    #[test]
+    fn rotate() {
+
+        
+        let p1 = Piece{colors:[C1,C2,C3,C4,C5,C6]};
+        let p2 = p1.rotate(South);
+        let p3 = p1.rotate(North);
+
+        println!("Pieza original: {:?}", p1 );
+        println!("Pieza rotada: {:?}", p3);
+
+        assert!(p1==p3);
+
+        assert!(!std::ptr::eq(&p2,&p3));        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn samePointer(){
+        let p = Piece{colors:[C1,C2,C3,C4,C5,C6]};
+        let rp1 = RotablePiece::from(p);
+        let rp2 = rp1.rotate(South).rotate(North);
+        assert!(std::ptr::eq(&rp1,&rp2));
+    }
+            
 }
